512eddff66eb088492c2ba053bc4f0631316acb4
nomilous <richard@clue.co.za>
2013-12-22 01:46:42 +0200

    okgood

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ dev (unreleased) version
 git seed clone / git seed pull
 
 # sudo npm install component -g
-component install
+component install -f  # force to get latest
 
 
 node_modules/vital/bin/vital -f example
--- example.js
+++ example.js
@@ -1,7 +1,11 @@
 
 // components required as node modules
 
-console.log( require('ubuntu-net-dev') );
-console.log( require('ubuntu-net-tcp') );
-console.log( require('ubuntu-net-udp') );
+// console.log( require('ubuntu-net-dev') );
+// console.log( require('ubuntu-net-tcp') );
+// console.log( require('ubuntu-net-udp') );
+
+net = require('ubuntu-net-dev');
+
+console.log( net() == 'okgood' );
 
